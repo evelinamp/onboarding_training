8bb1d996015e9b1faac91b75b4841d4a0a11ef44
Evelina <evelinabazouki@gmail.com>
2020-12-21 10:42:05 +0200

    deleted classes

--- src/main/java/com/onboarding/inventory/InventoryApplication.java
+++ src/main/java/com/onboarding/inventory/InventoryApplication.java
@@ -2,7 +2,7 @@ package com.onboarding.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
 
 
 @SpringBootApplication
